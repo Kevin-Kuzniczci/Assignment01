f48f2a7eb927552424913134a59f9b7e8bcb8a23
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-02-15 16:01:48 -0800

    Added steps
    Steps 1 through 20 done, just need to finish the last 2.

---
+++ js/script.js
@@ -0,0 +1,162 @@
+// Step 1
+
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+
+// Step 2
+
+// Numerical literal: 1
+// String: "Welcome to the Bay Area"
+// Boolean: true
+// literal: null
+
+
+// Step 3
+
+// x = 4;
+// y = x(5/2);
+
+
+// Step 4
+
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+
+// Step 5
+
+// let firstName = 'Bob';
+// var zipCode = 12345;
+// const mayWeContactYou = Maybe;
+
+
+// Step 6
+
+// let step6 = ' days until Taco Tuesday.';
+// let today = 6;
+
+// let example6 = today + step6;
+// console.log(example6)
+
+
+// Step 7
+
+// let Wednesday = false + " Wednesday";
+// let tomorrow = "Thursday " + true;
+
+// console.log(Wednesday);
+// console.log(tomorrow);
+
+
+// Step 8
+
+// It is not correct
+
+// let someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'"
+// console.log(someString)
+
+
+// Step 9
+
+// let nickName = null;
+// console.log(nickName);
+
+// let middleName;
+// console.log(middleName);
+
+
+// Step 10
+
+// let mystery;
+
+// const meal = {appetizer:"Salad with ghost pepper sauce", mainCourse:"All the processed food in the world", dessert:"None"};
+// let smorgasbord = "Happy Days" + 3 + true + meal + mystery;
+
+// console.log(smorgasbord);
+
+
+// Step 11
+
+// alert("Hello" + " Kevin Kuzniczci" + ", welcome to the JavaScript class!");
+
+
+// Step 12
+
+// let name = " Kevin Kuzniczci";
+
+// alert("Hello" + name + ", welcome to the JavaScript class!");
+
+
+// Step 13
+
+// let name = " Kevin Kuzniczci";
+// let course = " JavaScript";
+
+// alert("Hello" + name + ", welcome to the" + course + " class!");
+
+
+// Step 14
+
+// let name = " Kevin Kuzniczci";
+// let course = " JavaScript";
+
+// alert("Hello" + name + ", \nWelcome to the" + course + " class!");
+
+
+// Step 15
+
+// let name = prompt("What's your name?");
+// let course = " JavaScript";
+
+// alert("Hello " + name + ", \nWelcome to the" + course + " class!");
+
+
+// Step 16
+
+// let name = prompt("What's your name?");
+// let course = prompt("What class are you taking?");
+
+// alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+
+
+// Step 17
+
+// var x = 10;
+// var y = 20;
+
+// console.log(x + y);
+
+
+// Step 18
+
+// var x = 20;
+// console.log(x + 20);
+
+
+// Step 19
+
+// var x = 20;
+// console.log(x * 5);
+
+
+// Step 20
+
+// var x = 20 % 3;
+// console.log(x);
+
+
+// Step 21
+
+
+// Step 22
